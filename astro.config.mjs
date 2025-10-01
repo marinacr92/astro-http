9cb0a36f83e5756a154e9959be762f8d36a18795
Marina Caro <marina.caro@vasscompany.com>
2025-10-01 13:31:52 +0200

    feat: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
